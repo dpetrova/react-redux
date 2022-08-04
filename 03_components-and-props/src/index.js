39f857f227f76495e80267e5602d3ea63c2bdc79
dpetrova <daniella_petrova@abv.bg>
2022-08-04 17:17:58 +0300

    pass child component as a prop of parent component

--- 03_components-and-props/src/index.js
+++ 03_components-and-props/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Comment from "./Comment";
+import ApprovalCard from "./ApprovalCard";
 // a library to generate massive amounts of fake (but realistic) data for testing and development
 import { faker } from "@faker-js/faker";
 
@@ -10,24 +11,30 @@ const root = createRoot(container);
 const App = () => {
   return (
     <div className="ui container comments">
-      <Comment
-        author="Danny"
-        timeAgo="Today at 7:00PM"
-        content="Hello!"
-        avatar={faker.image.avatar()}
-      />
-      <Comment
-        author="Ben"
-        timeAgo="Today at 6:45PM"
-        content="Nice!"
-        avatar={faker.image.avatar()}
-      />
-      <Comment
-        author="John"
-        timeAgo="Today at 3:10AM"
-        content="Good morning!"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <Comment
+          author="Danny"
+          timeAgo="Today at 7:00PM"
+          content="Hello!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comment
+          author="Ben"
+          timeAgo="Today at 6:45PM"
+          content="Nice!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comment
+          author="John"
+          timeAgo="Today at 3:10AM"
+          content="Good morning!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
