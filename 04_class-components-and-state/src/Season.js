2c1d60fcf34293872a0e7de8a491472664ebfefc
dpetrova <daniella_petrova@abv.bg>
2022-08-07 10:55:28 +0300

    alternate state initialization, lifecycle methods

--- 04_class-components-and-state/src/Season.js
+++ 04_class-components-and-state/src/Season.js
@@ -1,7 +1,37 @@
 import React from "react";
 
+//determine the current season
+const getSeason = (lat, month) => {
+  //summer months in northern hemisphere (april-september)
+  //summer months in southern hemisphere (october-march)
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const seasonConfig = {
+  summer: {
+    text: "It is hot and sunny!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr, it is chilly!",
+    iconName: "snowflake",
+  },
+};
+
 const Season = (props) => {
-  return <div>Season display</div>;
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+  return (
+    <div>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
+    </div>
+  );
 };
 
 export default Season;
--- 04_class-components-and-state/src/index.js
+++ 04_class-components-and-state/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "semantic-ui-css/semantic.min.css"; //import css of Semantic UI framework
+import Season from "./Season";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -8,7 +10,9 @@ const root = createRoot(container);
 // Funcional component
 const App = () => {
   window.navigator.geolocation.getCurrentPosition(
+    //sucess callback
     (position) => console.log(position),
+    //error callback
     (err) => console.log(err)
   );
   return <div>Location: </div>;
@@ -18,28 +22,40 @@ const App = () => {
 // Class component
 class App extends React.Component {
   //constructor
-  constructor(props) {
-    // call parent class constructor to access the parent's properties and methods
-    super(props);
-    // inittialize state
-    this.state = { lat: null, errorMessage: "" };
+  // constructor(props) {
+  //   // call parent class constructor to access the parent's properties and methods
+  //   super(props);
+  //   // initialize state
+  //   this.state = { lat: null, errorMessage: "" };
+  // }
+
+  // short way to initialize state
+  state = { lat: null, errorMessage: "" };
+
+  // lifecycle methods
+  componentDidMount() {
+    console.log("The component was rendered to the screen");
     //get current location
     window.navigator.geolocation.getCurrentPosition(
-      //sucess callback
       (position) => {
         //update state (always with setState!!!!!!!!)
         this.setState({ lat: position.coords.latitude });
         //this.state.lat = position.coords.latitude; // DO NOT!!!!!!!!
       },
-      //error callback
       (err) => this.setState({ errorMessage: err.message })
     );
+  }
+  componentDidUpdate() {
+    console.log("The component was just updated and rerendered");
+  }
+  componentWillUnmount() {
+    console.log("The component will be no longer shown");
   }
 
   // render
   render() {
     if (this.state.lat && !this.state.errorMessage) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <Season lat={this.state.lat} />;
     }
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
