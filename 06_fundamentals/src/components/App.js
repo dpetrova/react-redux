8f1205aa7d8975c9bcd140309993800aeb1dc6d0
dpetrova <daniella_petrova@abv.bg>
2022-08-20 17:44:19 +0300

    youtube like app fuctionality

--- 06_fundamentals/src/components/App.js
+++ 06_fundamentals/src/components/App.js
@@ -1,20 +1,49 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-  onSearchSubmit = (searchTerm) => {
-    youtube.get("/search", {
+  state = { videos: [], selectedVideo: null };
+  onSearchSubmit = async (searchTerm) => {
+    const response = await youtube.get("/search", {
       params: {
         q: searchTerm,
       },
     });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  componentDidMount() {
+    this.onSearchSubmit("universe");
+  }
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onSearchSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
