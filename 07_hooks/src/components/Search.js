73da94943c1bd318696f82b50644c9d7f78ac8b1
dpetrova <daniella_petrova@abv.bg>
2022-08-24 11:11:49 +0300

    direct render HTML into JSX

--- 07_hooks/src/components/Search.js
+++ 07_hooks/src/components/Search.js
@@ -4,22 +4,47 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
 
-  /* useEffect is a function that takes 2 arguments: first -> is a function; second -> control when the function is executed */
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    if (term) search();
+  }, [term]);
+
+  /* useEffect is a function that takes 2 arguments: first -> is a function; second -> control when the function is executed 
+
   //run at initial render
   useEffect(() => {
     console.log("I ONLY RUN ONCE AT INITIAL RENDER");
   }, []);
+
   //run at initial render; and run after every rerender
   useEffect(() => {
     console.log("I RUN WITH EVERY RENDER");
   });
+
   //run at initial render; and run after every rerender if term has chanded since last render
   useEffect(() => {
     console.log("I RUN WITH EVERY RENDER IF term DATA IS CHANGED");
   }, [term]);
 
-  /* using async functions with useEffect */
+  */
+
+  /* using async functions with useEffect 
+
   //using async helper function
   useEffect(() => {
     const search = async () => {
@@ -27,16 +52,41 @@ const Search = () => {
     };
     search();
   }, [term]);
+
   //using IIFE helper function
   useEffect(() => {
     (async () => {
       const response = await axios.get("url");
     })();
   }, [term]);
+
   //using promises
   useEffect(() => {
     axios.get("url").then((response) => {});
   }, [term]);
+
+  */
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            target="_blank"
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {/* direct render html (it is dangerous because of XSS attack) */}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <React.Fragment>
@@ -50,6 +100,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </React.Fragment>
   );
 };
