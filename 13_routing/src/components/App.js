ba355a4be4b4f1d54980f9eaa4139bb109e47cc8
dpetrova <daniella_petrova@abv.bg>
2023-05-31 18:58:37 +0300

    react router

---
+++ 13_routing/src/components/App.js
@@ -0,0 +1,73 @@
+import React from "react";
+import {
+  BrowserRouter,
+  HashRouter,
+  MemoryRouter,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const PageOne = () => {
+  return <div>Page One</div>;
+};
+const PageTwo = () => {
+  return <div>Page Two</div>;
+};
+
+const App = () => {
+  return (
+    <div className="ui container">
+      {/* BrowserRouter uses everything after TLD (.com, .net) or port as path  */}
+      <BrowserRouter>
+        <div>
+          {/* menu */}
+          <div className="ui secondary pointing menu">
+            <Link to="/" className="item">
+              Page One
+            </Link>
+            <Link to="/pagetwo" className="item">
+              Page Two
+            </Link>
+          </div>
+          {/* page */}
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+
+      {/* HashRouter uses everything after # as path  */}
+      {/* <HashRouter>
+        <div>
+          <div className="ui secondary pointing menu">
+            <Link to="/" className="item">
+              Page One
+            </Link>
+            <Link to="/pagetwo" className="item">
+              Page Two
+            </Link>
+          </div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </HashRouter> */}
+
+      {/* MemoryRouter does not use the URL to track navigation  */}
+      {/* <MemoryRouter>
+        <div>
+          <div className="ui secondary pointing menu">
+            <Link to="/" className="item">
+              Page One
+            </Link>
+            <Link to="/pagetwo" className="item">
+              Page Two
+            </Link>
+          </div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </MemoryRouter> */}
+    </div>
+  );
+};
+
+export default App;
