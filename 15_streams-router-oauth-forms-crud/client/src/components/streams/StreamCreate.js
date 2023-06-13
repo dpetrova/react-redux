9fb594656200cb41b9142bb0f6c22cd43ad830d0
dpetrova <daniella_petrova@abv.bg>
2023-06-13 12:07:37 +0300

    handling forms with redux form

--- 15_streams-router-oauth-forms-crud/client/src/components/streams/StreamCreate.js
+++ 15_streams-router-oauth-forms-crud/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends Component {
+  renderInput = (formProps) => {
+    // console.log(formProps);
+    return (
+      // <input
+      //   onChange={formProps.input.onChange}
+      //   value={formProps.input.value}
+      // />
+      <div
+        className={`field ${
+          formProps.meta.error && formProps.meta.touched ? "error" : ""
+        }`}
+      >
+        {/* label */}
+        <label>{formProps.label}</label>
+        {/* input field */}
+        <input {...formProps.input} autoComplete="off" />
+        {/* validation error message */}
+        {this.renderError(formProps.meta)}
+      </div>
+    );
+  };
+
+  renderError(metaProps) {
+    if (metaProps.touched && metaProps.error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{metaProps.error}</div>
+        </div>
+      );
+    }
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <form
+        className="ui form error"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field name="title" component={this.renderInput} label="Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "Title is required!";
+  }
+  if (!formValues.description) {
+    errors.description = "Description is required!";
+  }
+
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate",
+  validate: validate,
+})(StreamCreate);
--- 15_streams-router-oauth-forms-crud/client/src/index.js
+++ 15_streams-router-oauth-forms-crud/client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 // import thunk from "redux-thunk";
 
 import App from "./components/App";
@@ -9,7 +9,12 @@ import reducers from "./reducers";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const store = createStore(reducers /*applyMiddleware(thunk) */);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  /* preloadedState, */
+  composeEnhancers(applyMiddleware(/*...middleware*/))
+);
 
 root.render(
   <Provider store={store}>
