0fa5caab5a40bde4d3e7e8a22d88d53d4c20bda4
dpetrova <daniella_petrova@abv.bg>
2023-06-23 12:05:56 +0300

    migrate from redux-form to react-final-form

--- 15_streams-router-oauth-forms-crud/client/src/components/streams/StreamForm.js
+++ 15_streams-router-oauth-forms-crud/client/src/components/streams/StreamForm.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
-import { Field, reduxForm } from "redux-form";
-// import { connect } from "react-redux";
-// import { createStream } from "../../actions";
+import React from "react";
+import { Form, Field } from "react-final-form";
 
-class StreamForm extends Component {
-  renderInput = (formProps) => {
+const StreamForm = (props) => {
+  const renderInput = (formProps) => {
     // console.log(formProps);
     return (
       // <input
@@ -21,12 +19,12 @@ class StreamForm extends Component {
         {/* input field */}
         <input {...formProps.input} autoComplete="off" />
         {/* validation error message */}
-        {this.renderError(formProps.meta)}
+        {renderError(formProps.meta)}
       </div>
     );
   };
 
-  renderError(metaProps) {
+  const renderError = (metaProps) => {
     if (metaProps.touched && metaProps.error) {
       return (
         <div className="ui error message">
@@ -34,53 +32,40 @@ class StreamForm extends Component {
         </div>
       );
     }
-  }
-
-  onSubmit = (formValues) => {
-    // console.log(formValues);
-    // this.props.createStream(formValues);
-    this.props.onSubmit(formValues);
   };
 
-  render() {
-    console.log(this.props);
-    return (
-      <form
-        className="ui form error"
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-      >
-        <Field name="title" component={this.renderInput} label="Title" />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Description"
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
-    );
-  }
-}
+  const onSubmit = (formValues) => {
+    // console.log(formValues);
+    props.onSubmit(formValues);
+  };
 
-const validate = (formValues) => {
-  const errors = {};
-  if (!formValues.title) {
-    errors.title = "Title is required!";
-  }
-  if (!formValues.description) {
-    errors.description = "Description is required!";
-  }
-
-  return errors;
+  return (
+    <Form
+      initialValues={props.initialValues}
+      onSubmit={onSubmit}
+      validate={(formValues) => {
+        const errors = {};
+        if (!formValues.title) {
+          errors.title = "Title is required!";
+        }
+        if (!formValues.description) {
+          errors.description = "Description is required!";
+        }
+        return errors;
+      }}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit} className="ui form error">
+          <Field name="title" component={renderInput} label="Title" />
+          <Field
+            name="description"
+            component={renderInput}
+            label="Description"
+          />
+          <button className="ui button primary">Submit</button>
+        </form>
+      )}
+    />
+  );
 };
 
-export default reduxForm({
-  form: "streamForm",
-  validate: validate,
-})(StreamForm);
-
-// const formWrapped = reduxForm({
-//   form: "streamForm",
-//   validate: validate,
-// })(StreamForm);
-
-// export default connect(null, { createStream })(formWrapped);
+export default StreamForm;
--- 15_streams-router-oauth-forms-crud/client/src/reducers/index.js
+++ 15_streams-router-oauth-forms-crud/client/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
 import streamReducer from "./streamReducer";
 
 export default combineReducers({
-  form: formReducer,
   auth: authReducer,
   streams: streamReducer,
 });
